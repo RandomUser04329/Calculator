2b2bedefd30eeca3a9dea25f0c1be5783166f432
2sided <rsalinas24z@yahoo.com>
2025-07-28 12:47:19 -0500

    Working with arrays for the numbers.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
-
+//Input Field 
 const numField = document.querySelector("#numField");
 
 //Add, Subtract, Multiply, Divide, Decimal buttons
 const divide = document.querySelector("#divide");
-const multiply = document.querySelector("multiply");
+const multiply = document.querySelector("#multiply");
 const subtract = document.querySelector("#subtract");
 const add = document.querySelector("#add");
 const decimal = document.querySelector("#decimal");
 
 //Clear button
-const clear = document.querySelector("#clear");
+const backSpace = document.querySelector("#clear");
 
 // The Number buttons
 const nine = document.querySelector("#nine");
@@ -23,100 +23,114 @@ const two = document.querySelector("#two");
 const one = document.querySelector("#one");
 const zero = document.querySelector("#zero");
 
+//Equal sign button 
+const equal = document.querySelector("#equals");
 
-function inputField(num) {
+let storedEquation = [];
 
-    let btnValue;
+function createEquation(int) {
+    let operator; 
 
-    if (num === "+" || num === "-" || num === "x" || num === "/") {
-        btnValue != num;
-        return secondInput(num, numField.value);
-    } else if (num === ".") {
-        btnValue = num; 
+    if (int === "+" || int === "-" || int === "*" || int === "/") {
+        operator = int;
     } else { 
-        btnValue = parseInt(num);
+        numField.value += int;
     }
 
-     numField.value += btnValue;
+    let values = numField.value;
+    let equation = []; 
+    let index = 0;
 
-     console.log(numField.value);
-
-    clear.addEventListener("click", function () {
-        numField.value = "";
-        return;
-    })
-}
-
-function secondInput(symbol, firstNumToSolve) {
-
-    if (firstNumToSolve.includes(".")) {
-        firstNumToSolve = parseInt(firstNumToSolve.value);
+    if (operator === "+" || operator === "-" || operator === "*" || operator === "/") {
+        index++; 
+        storedEquation[index] = [equation];
+        values = "";
     }
 
-    
+    equation[index] = [values];
+    storedEquation[index] = [equation];
+
+
+    for (let i = 0; i < numField.value.length; i++) {
+        console.log(equation); 
+        console.log(storedEquation);
+    }
+
+
 }
 
 
-// NUMBER BUTTONS
+//Number buttons
 nine.addEventListener("click", function() {
-    inputField(9);
+    createEquation(9);
 });
 
 eight.addEventListener("click", function() {
-    inputField(8);
+    createEquation(8);
 });
 
 seven.addEventListener("click", function() {
-    inputField(7);
+    createEquation(7);
 });
 
 six.addEventListener("click", function() {
-    inputField(6);
+    createEquation(6);
 });
 
 five.addEventListener("click", function() {
-    inputField(5);
+    createEquation(5);
 });
 
 four.addEventListener("click", function() {
-    inputField(4);
+    createEquation(4);
 });
 
 three.addEventListener("click", function() {
-    inputField(3);
+    createEquation(3);
 });
 
 two.addEventListener("click", function() {
-    inputField(2);
+    createEquation(2);
 });
 
 one.addEventListener("click", function() {
-    inputField(1);
+    createEquation(1);
 });
 
 zero.addEventListener("click", function() {
-    inputField(0);
+    createEquation(0);
 });
 
+
+//Decimal button
 decimal.addEventListener("click", function() {
-    inputField(".");
+    createEquation(".");
 });
 
-//ARITHMETRIC BUTTONS
-add.addEventListener("click", function() {
 
-});
-
-subtract.addEventListener("click", function() {
-
+//Operator buttons
+divide.addEventListener("click", function() {
+    createEquation("/");
 });
 
 multiply.addEventListener("click", function() {
+    createEquation("*");
+});
+
+subtract.addEventListener("click", function() {
+    createEquation("-");
+});
+
+add.addEventListener("click", function() {
+    createEquation("+");
+});
+
+//Backspace button
+backSpace.addEventListener("click", function() {
 
 });
 
-divide.addEventListener("click", function() {
+//Equals sign button
+equal.addEventListener("click", function() {
 
 });
-
-
