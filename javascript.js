13413d55baf7ff9429a332b4e390c13151daecac
Rob <rsalinas24z@yahoo.com>
2025-08-19 11:08:14 -0700

    Finished product. Ready for Feedback

--- javascript.js
+++ javascript.js
@@ -31,15 +31,7 @@ let currentNum = "";
 const operators = ["/", "*", "-", "+"];
 
 function equation(input) { 
-
-    //Changes the clear button text content
-    backSpace.textContent = "C";
-
-    if (numField.value === "") { 
-        backSpace.textContent = "AC";
-    }
-
-
+    
     //If one of the buttons contains an operator string, it clears the field but inserts in an array
    if (operators.includes(input)) { 
 
@@ -55,13 +47,28 @@ function equation(input) {
         three elements in the array so that it shows the current answer to the user 
         but will allow them to continue their equation*/
 
-        if (!input.includes("=")) { //FINALLLYLYYYYYY GOT ITTT
+        if (!input.includes("=")) { 
             let val1;
             let val2;
             let sum; 
            for (let i = 0; i < equationArr.length; i++) {
                 val1 = equationArr[0];  
                 val2 = equationArr[2];
+
+                //ERROR Check
+                if (operators.includes(val1)) {
+                    console.log("Entered an operator with no value previously, Restarting.")
+                    equationArr = [];
+                    numField.value = "";
+                    return;
+                }
+                if (operators.includes(val2)) {
+                    console.log("Entered an operator with no value previously, Restarting.")
+                    equationArr = [];
+                    numField.value = "";
+                    return;
+                }
+
                 if (equationArr.length > 3) { 
                     if (equationArr[1] === "/") { 
                         sum = Number(val1) / Number(val2);
@@ -117,8 +124,8 @@ function equation(input) {
             equationArr = [];
             currentNum = "";
         }
-
-        return numField.value = finalSum;
+        numField.value = finalSum;
+        return equationArr.push(finalSum);
 
     } else { 
         // takes the values and concates them into a string
@@ -127,7 +134,34 @@ function equation(input) {
     // which is then concated to the input field value
    numField.value = currentNum;
 
-   //console.log(equationArr);
+    //Checks for decimal count in within the text field
+   let decimalCount = 0;
+   for (let i = 0; i < numField.value.length; i++) { 
+        if (numField.value.charAt(i) === ".") { 
+            decimalCount++;
+        }
+        
+        if (decimalCount > 1) { 
+            alert("No more than one decimal in a number.");
+            currentNum = currentNum.substring(0, currentNum.length - 1);
+            numField.value = numField.value.substring(0, numField.value.length - 1);
+        }
+   }
+    
+   //Changes the clear button text content
+   
+    if (numField.value.length > 0) { 
+        backSpace.textContent = "C";
+    } 
+
+    if (numField.value.length <= 0 || numField.value === "") {
+        backSpace.textContent = "AC";
+    }
+        
+    
+   console.log(equationArr);
+   console.log(currentNum);
+   console.log(numField.value);
 
 }
 
@@ -135,10 +169,25 @@ function equation(input) {
 //BACKSPACE OR CLEARALL
 
 function backSpaceHandler() {
-    
-    if (currentNum.length > 0) {
-        return numField.value = currentNum.substring(0, currentNum.length - 1);
-    }   
+
+    //If the clear button is just the clear text, it only takes one character out from the field
+    if (backSpace.textContent === "C") { 
+        currentNum = currentNum.substring(0, currentNum.length - 1)
+        numField.value = numField.value.substring(0, numField.value.length - 1)
+    //if the button text is AC, then it clears the array, numField and currentNum 
+    } else if (backSpace.textContent === "AC") { 
+        currentNum = "";
+        equationArr = []; 
+        numField.value = "";
+    }
+
+
+    //Checks if the length of the input field is either empty or not, then changes the clear buttons text content
+    if (numField.value.length === 0) { 
+        backSpace.textContent = "AC";
+    } else { 
+        backSpace.textContent = "C";
+    }
 }
 
 
